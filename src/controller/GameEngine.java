cd24fa8af3aaaf2056ecdf3984f6b6c4736bf23d
SantiaGoMode <cris.santiago@knights.ucf.edu>
2019-12-02 18:00:26 -0500

    Free Online Hosting

--- src/controller/GameEngine.java
+++ src/controller/GameEngine.java
@@ -16,10 +16,10 @@ public class GameEngine {
 	public static Connection getConnection() {
 		// Database Initial Connection, will have to be called for each class
 		
-		String username = "oldtown";
-		String pass = "road";
+		String username = "sql9314118";
+		String pass = "fBAhsdUvtm";
 		
-		String url = "jdbc:mysql://localhost:3306/horseracing";
+		String url = "jdbc:mysql://sql9.freemysqlhosting.net:3306/sql9314118";
 		try {
 			Connection conn = DriverManager.getConnection(url,username,pass);
 			System.out.println("Connected");
