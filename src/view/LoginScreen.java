4a04c8b89670044df054cc97427e707da9f2a6ed
theowenanderson <46496020+theowenanderson@users.noreply.github.com>
2019-12-03 20:31:48 -0500

    fixed like a lot of error-filled code from last commit :)

--- src/view/LoginScreen.java
+++ src/view/LoginScreen.java
@@ -2,7 +2,7 @@ package view;
 
 
 import java.awt.BorderLayout;
-import java.awt.Color;
+
 import java.sql.*;
 import java.awt.EventQueue;
 
@@ -17,22 +17,17 @@ import javax.swing.JOptionPane;
 import javax.swing.JTextField;
 import java.awt.Font;
 import javax.swing.JPasswordField;
-import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.Statement;
-import java.awt.Panel;
 
 
 
 public class LoginScreen extends JFrame {
 
-	private JPanel panel;
-	private JTextField textField;
-	private JPasswordField passwordField;
+	private JPanel contentPane;
+	private JTextField user;
+	private JPasswordField pass;
 
 	/**
 	 * Launch the application.
@@ -54,85 +49,40 @@ public class LoginScreen extends JFrame {
 	 * Create the frame.
 	 */
 	public LoginScreen() {
-		setLayout(null);
-		setTitle("Login!");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		JPanel panel = new JPanel();
-		panel.setBackground(new Color(153, 255, 255));
-		setBounds(100, 100, 400, 300);
-		panel.setBounds(6, 6, 393, 289);
-		add(panel);
-		panel.setLayout(null);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
 		
-		JButton btnNewButton = new JButton("Login");
-		btnNewButton.setBounds(109, 233, 79, 29);
-		panel.add(btnNewButton);
+		JLabel lblLoginPage = new JLabel("Username");
+		lblLoginPage.setBounds(20, 57, 67, 24);
+		contentPane.add(lblLoginPage);
 		
-		JButton button = new JButton("Register");
-		button.setBounds(200, 233, 79, 29);
-		panel.add(button);
+		user = new JTextField();
+		user.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		user.setBounds(10, 80, 211, 31);
+		contentPane.add(user);
+		user.setColumns(10);
 		
-		button.addActionListener(new ActionListener() {
+		JLabel label = new JLabel("Login Page");
+		label.setFont(new Font("Tahoma", Font.PLAIN, 28));
+		label.setBounds(10, 11, 148, 40);
+		contentPane.add(label);
+		
+		JLabel lblPassword = new JLabel("Password");
+		lblPassword.setBounds(20, 134, 67, 24);
+		contentPane.add(lblPassword);
+		
+		pass = new JPasswordField();
+		pass.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		pass.setBounds(10, 157, 211, 31);
+		contentPane.add(pass);
+		
+		JButton btnLogin = new JButton("Login");
+		btnLogin.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				RegisterScreen nc = new RegisterScreen();
-				nc.setVisible(true);
-				
-			}
-		});
-		
-		passwordField = new JPasswordField();
-		passwordField.setBounds(164, 183, 130, 26);
-		panel.add(passwordField);
-		
-		JLabel label = new JLabel("Password");
-		label.setBounds(80, 183, 72, 27);
-		panel.add(label);
-		
-		JLabel lblUsername = new JLabel("Username");
-		lblUsername.setBounds(80, 144, 72, 27);
-		panel.add(lblUsername);
-		
-		textField = new JTextField();
-		textField.setBounds(164, 144, 130, 26);
-		panel.add(textField);
-		textField.setColumns(10);
-		
-		JLabel lblMystical = new JLabel("Mystical");
-		lblMystical.setFont(new Font("Noteworthy", Font.PLAIN, 17));
-		lblMystical.setBounds(251, 6, 72, 20);
-		panel.add(lblMystical);
-		
-		JLabel label_1 = new JLabel("Horse");
-		label_1.setFont(new Font("Noteworthy", Font.PLAIN, 17));
-		label_1.setBounds(261, 25, 72, 20);
-		panel.add(label_1);
-		
-		JLabel label_2 = new JLabel("Racing");
-		label_2.setFont(new Font("Noteworthy", Font.PLAIN, 17));
-		label_2.setBounds(271, 41, 72, 29);
-		panel.add(label_2);
-		
-		JLabel lblNewLabel = new JLabel("New label");
-		lblNewLabel.setIcon(new ImageIcon("img/charlie3.jpg"));
-		lblNewLabel.setBounds(23, 6, 353, 126);
-		panel.add(lblNewLabel);
-		btnNewButton.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-<<<<<<< HEAD
-					Connection conn = GameEngine.getConnection();
-					String username = passwordField.getText();
-					String password = textField.getText();
-					try {
-						//Class.forName("com.mysql.jdbc.Driver");
-						
-						Statement stmt = conn.createStatement();
-						String sql = "Select * from users where username='"+username+"' and Password='"+password+"'";					
-						ResultSet rs = stmt.executeQuery(sql);
-						if(rs.next()) {
-							JOptionPane.showMessageDialog(null, "Login Successful");
-							GameEngine.username_current = username;
-							sql = "Select id from users where username='"+username+"'";
-=======
 				Connection conn = GameEngine.getConnection();
 				String username = user.getText();
 				String password = pass.getText();
@@ -150,33 +100,40 @@ public class LoginScreen extends JFrame {
 						if(rs.next()) {
 							GameEngine.userID_current = rs.getInt("id");
 							sql = "Select balance from users where username='"+username+"'";
->>>>>>> b90656c90ce8c27e4ee1a199caa6fcfded36a674
 							rs = stmt.executeQuery(sql);
 							if(rs.next()) {
-								GameEngine.userID_current = rs.getInt("id");
-								sql = "Select balance from users where username='"+username+"'";
-								rs = stmt.executeQuery(sql);
-								if(rs.next()) {
-									GameEngine.userIDbalance = rs.getInt("balance");
-									
-								}
-								else System.out.println("error with obtaining balance");
+								GameEngine.userIDbalance = rs.getInt("balance");
 								
 							}
-		
-							else System.out.println("error with obtaining user id");
-							Menu men = new Menu();
-							men.setVisible(true);
-							dispose();
+							else System.out.println("error with obtaining balance");
+							
 						}
-						else {
-							JOptionPane.showMessageDialog(null, "User not found");
-						}
-					}catch(Exception e1) {
-						System.out.print(e);
+	
+						else System.out.println("error with obtaining user id");
+						Menu men = new Menu();
+						men.setVisible(true);
+						dispose();
 					}
+					else {
+						JOptionPane.showMessageDialog(null, "User not found");
+					}
+				}catch(Exception e1) {
+					System.out.print(e);
 				}
-			});
-
+			}
+		});
+		btnLogin.setBounds(20, 199, 89, 23);
+		contentPane.add(btnLogin);
+		
+		JButton btnRegister = new JButton("Register");
+		btnRegister.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				RegisterScreen nc = new RegisterScreen();
+				nc.setVisible(true);
+				
+			}
+		});
+		btnRegister.setBounds(132, 199, 89, 23);
+		contentPane.add(btnRegister);
 	}
 }
